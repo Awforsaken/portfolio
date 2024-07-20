fa8c30b24bc50978feb2089d9e1fe5c3f215acd0
Awforsaken <d.maskolaitis@gmail.com>
2024-07-20 19:11:47 +0100

    added contents

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# portfolio
-Portfolio page
+# Welcome to [WhatFor](https://www.whatfor.dev/)
+
+## 👀 Want to get to know me?
+
+Feel free to get in touch on GitHub!
---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  integrations: [react(), tailwind(), sitemap()],
+  output: "static",
+  adapter: vercel(),
+});
