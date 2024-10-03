849e1caa8f6ded20a7423558e0c75ccc1643f69a
Awforsaken <d.maskolaitis@gmail.com>
2024-10-03 14:11:20 +0100

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
+  output: 'static',
   adapter: vercel(),
+  integrations: [react()]
 });
