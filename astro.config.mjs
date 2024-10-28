269b13ec9ad4110900e848711a21bcee28cd6f72
Awforsaken <d.maskolaitis@gmail.com>
2024-10-28 18:43:56 +0000

    Remove react deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   adapter: vercel(),
-  integrations: [react()]
+  integrations: []
 });
