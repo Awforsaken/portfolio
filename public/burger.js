58cec09bd1fe8019e61d14957c8799924cdd7dd4
Awforsaken <d.maskolaitis@gmail.com>
2024-11-06 19:43:18 +0000

    made start on mobile navigation and cleaned up styles for project pages

--- public/burger.js
+++ public/burger.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#mobile-menu-button').click(function() {
-      $('.mobile-menu').slideToggle();
+      $('.mobile-menu').toggleClass('open');
+      $('#mobile-menu-button .cta, #mobile-menu-button .close').toggleClass("hidden")
       console.log('burger');
     });
 
