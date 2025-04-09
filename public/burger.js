c40d5280c95cb8c49d572ecf728cbaf341bd2ed2
Awforsaken <d.maskolaitis@gmail.com>
2025-04-09 19:42:38 +0100

    added breadcrums, tweaked tags and clicking on background of mobile nav now closes nav

--- public/burger.js
+++ public/burger.js
@@ -5,4 +5,9 @@ $(document).ready(function() {
       console.log('burger');
     });
 
+    $(document).on('click', '.mobile-menu.open', function() {
+      $('.mobile-menu').toggleClass('open');
+      $('#mobile-menu-button .cta, #mobile-menu-button .close').toggleClass("hidden");
+      console.log('menu clicked');
+    });
   });
